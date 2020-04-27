ed4f1e835a8c5364666c5207a97268a603c7e6db
miniferretti <36267317+miniferretti@users.noreply.github.com>
2020-04-27 22:17:45 +0200

    Update slider_PID.py

--- Python_code/slider_PID.py
+++ Python_code/slider_PID.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
@@ -57,33 +57,33 @@ w6.pack()
 Button(master, text='Update PID', command=print_values_in_file).pack()
 
 
-fig, axs = plt.subplots(2, 1, figsize=(20,20))
+fig, axs = plt.subplots(2, 1, figsize=(20, 20))
 
 
 def animate(i):
-    with open('/home/pi/Poseitron-Code/Data/logFileSpeed.txt', 'r') as f:
-        next(f)
-        Vr = []
-        VrRef = []
-        Vl = []
-        VlRef = []
-        Time = []
-        for line in f:
-            line = line.strip()
-            vr, vrref, vl, vlref, time = line.split()
-            Vr.append(float(vr))
-            VrRef.append(float(vrref))
-            Vl.append(float(vl))
-            VlRef.append(float(vlref))
-            Time.append(float(time))
-        axs[0].clear()
-        axs[0].plot(Time, Vr)
-        axs[0].plot(Time, VrRef)
-        axs[0].set_title("Right Motor Speed VS reference")
-        axs[1].clear()
-        axs[1].plot(Time, Vl)
-        axs[1].plot(Time, VlRef)
-        axs[1].set_title("Left Motor Speed VS reference")
+    f = open('/home/pi/Poseitron-Code/Data/logFileSpeed.txt', 'r')
+    next(f)
+    Vr = []
+    VrRef = []
+    Vl = []
+    VlRef = []
+    Time = []
+    for line in f:
+        line = line.strip()
+        vr, vrref, vl, vlref, time = line.split()
+        Vr.append(float(vr))
+        VrRef.append(float(vrref))
+        Vl.append(float(vl))
+        VlRef.append(float(vlref))
+        Time.append(float(time))
+    axs[0].clear()
+    axs[0].plot(Time, Vr)
+    axs[0].plot(Time, VrRef)
+    axs[0].set_title("Right Motor Speed VS reference")
+    axs[1].clear()
+    axs[1].plot(Time, Vl)
+    axs[1].plot(Time, VlRef)
+    axs[1].set_title("Left Motor Speed VS reference")
 
 
 chart_type = FigureCanvasTkAgg(fig, master)
