cd51237904e6fd44f5f29b766b5a2a1f1816b007
miniferretti <36267317+miniferretti@users.noreply.github.com>
2020-04-27 16:07:44 +0200

    Update slider_demo.py

--- Python_code/slider_demo.py
+++ Python_code/slider_demo.py
@@ -4,10 +4,10 @@ def show_values():
     print (w1.get(), w2.get())
 
 master = Tk()
-w1 = Scale(master, from_=0, to=1,length=600,digits=4,resolution=0.0001)
+w1 = Scale(master, from_=0, to=1,length=600,digits=4,resolution=0.0001,orient=HORIZONTAL)
 w1.set(0)
 w1.pack()
-w2 = Scale(master, from_=0, to=1, length=600,digits=4,resolution=0.0001)
+w2 = Scale(master, from_=0, to=1, length=600,digits=4,resolution=0.0001,orient=HORIZONTAL)
 w2.set(0)
 w2.pack()
 Button(master, text='Show', command=show_values).pack()
