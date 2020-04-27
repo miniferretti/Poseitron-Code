e695ae8a925118930115f4cfe7b528506227b48f
miniferretti <36267317+miniferretti@users.noreply.github.com>
2020-04-27 16:13:33 +0200

    Update slider_demo.py

--- Python_code/slider_demo.py
+++ Python_code/slider_demo.py
@@ -1,15 +1,15 @@
 from Tkinter import *
 
-def show_values():
+def print_values_in_file():
     print (w1.get(), w2.get())
 
 master = Tk()
-w1 = Scale(master, from_=0, to=1,length=600,digits=4,resolution=0.0001,orient=HORIZONTAL)
+w1 = Scale(master, from_=0, to=1,length=600,digits=4,resolution=0.0001,orient=HORIZONTAL,label='Kp')
 w1.set(0)
 w1.pack()
-w2 = Scale(master, from_=0, to=1, length=600,digits=4,resolution=0.0001,orient=HORIZONTAL)
+w2 = Scale(master, from_=0, to=1, length=600,digits=4,resolution=0.0001,orient=HORIZONTAL, label='ki')
 w2.set(0)
 w2.pack()
-Button(master, text='Show', command=show_values).pack()
+Button(master, text='Show', command=print_values_in_file).pack()
 
 mainloop()
