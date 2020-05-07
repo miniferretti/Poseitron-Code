bc2d6deb3351706f81006e673f23de028e6ac2ef
miniferretti <36267317+miniferretti@users.noreply.github.com>
2020-05-07 18:41:59 +0200

    test

--- C_code/IO/MCP23017_FPGA/Pinchers_control.cc
+++ C_code/IO/MCP23017_FPGA/Pinchers_control.cc
@@ -1,14 +1,14 @@
 #include "Pinchers_control.hh"
 
-void set_pinchers_output(unsigned char A, unsigned char B){
+void set_pinchers_output(unsigned char A, unsigned char B)
+{
 
-unsigned char buffer[5];
-buffer[4]=PINCHER_REG;
-buffer[3]=0x00;
-buffer[2]=0x00;
-buffer[1]=B;
-buffer[0]=A; 
+    unsigned char buffer[5];
+    buffer[4] = PINCHER_REG;
+    buffer[3] = 0x00;
+    buffer[2] = 0x00;
+    buffer[1] = B;
+    buffer[0] = A;
 
-wiringPiSPIDataRW(0,buffer,5);
-
+    wiringPiSPIDataRW(0, buffer, 5);
 }
